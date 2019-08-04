58c3bf98b283fdaddd34b08314434f42015354d1
Balmukund jha <mukund2401@gmail.com>
2019-08-05 00:03:27 +0530

    test

---
+++ engine/ajax/updates.php
@@ -0,0 +1,7 @@
+<?php
+echo <<<HTML
+<div class="ui-state-error ui-corner-all" style="padding:10px;"><b>Attention!</b>
+<br />To avoid problems, for security reasons, check for updates is disabled!
+</div>
+HTML;
+?>
